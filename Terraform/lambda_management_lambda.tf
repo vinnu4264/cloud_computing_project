6849aa84078d4d072b63b874127127aeabdd08f0
Sharath <user.email=vinnu4264@gmail.com>
2022-07-30 20:05:25 -0400

    Testing code for lambda creation

---
+++ Terraform/lambda_management_lambda.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "lambdaiam" {
+  name = "lambdaiam"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "cc_management_lambda" {
+  function_name = "cc_management_lambda"
+  role          = aws_iam_role.lambdaiam.arn
+  handler       = "cc_manage_lambda.main.lambda_handler"
+#   layers = [ aws_lambda_layer_version.common_lambda_layer.id ]
+  timeout = "300"
+
+  source_code_hash = filebase64sha256("./bin/cc_manage_lambda.zip")
+
+  runtime = "python3.7"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
