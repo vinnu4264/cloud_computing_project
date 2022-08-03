6340a926b7930303c7536c1ade6f4fdbeae50f9d
Sharath <user.email=vinnu4264@gmail.com>
2022-08-03 01:31:37 -0400

    Fixed lambda zip

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -5,5 +5,5 @@ data "aws_ecr_authorization_token" "token"{}
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
   source_file = "${path.module}/code/lambda_function.py"
-  output_path = "nametest.zip"
+  output_path = "management_lambda.zip"
 }
