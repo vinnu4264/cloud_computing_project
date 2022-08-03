c7fe85ad89b39f78b5b8c7b843f9f5a01fada643
Sharath <user.email=vinnu4264@gmail.com>
2022-08-03 01:24:00 -0400

    Fixed providers

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -4,8 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.24.0"
     }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+    
   }
   backend "s3" {
+    region = "us-east-1"
     bucket = "cloud-computing-terraform-state-files"
     key    = "stateFiles/terraform.tfstate"
   }
