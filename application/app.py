00b38be0ea4810fd01ebe2d6620a746801c58b9f
Sharath <user.email=vinnu4264@gmail.com>
2022-08-08 22:30:18 -0400

    Working code for application ECS

--- application/app.py
+++ application/app.py
@@ -2,35 +2,39 @@ from flask import Flask
 import socket
 from time import sleep
 from random import randint
+from data_collector import collect_data
 
 app = Flask(__name__)
 
+@app.route('/')
+def tester():
+    d = collect_data()
+    stats_data = d.hammer()
+    return stats_data
 
-def sleep_custom():
-    sleep(randint(1, 9))
 
-@app.route('/<test>')
+@app.route('/custom/<test>')
 def sayHello(test):
     if test=="t1":
-        sleep_custom()
+        
         return {
             "Name": f"{socket.gethostname()}",
             "Test": "Called t1"
         }
     elif test=="t2":
-        sleep_custom()
+        
         return {
             "Name": f"{socket.gethostname()}",
             "Test": "Called t2"
         }
     elif test=="t3":
-        sleep_custom()
+        
         return {
             "Name": f"{socket.gethostname()}",
             "Test": "Called t3"
         }
     else:
-        sleep_custom()
+        
         return {
             "Name": f"{socket.gethostname()}",
             "Test": "Called Other"
@@ -39,5 +43,5 @@ def sayHello(test):
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    # app.run(debug=True, port=3000)
+    # app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, port=4040)
