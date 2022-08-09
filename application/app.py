d42bac33a3a673623242c45f3ffaade3593d4434
Sharath <user.email=vinnu4264@gmail.com>
2022-08-08 22:49:37 -0400

    Fix: Port of container

--- application/app.py
+++ application/app.py
@@ -46,5 +46,5 @@ def sayHello(test):
     
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True, port=4040)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True, port=4040)
