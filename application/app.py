7367a1af3c3eda90b1c08715eff319aed11ca643
Sharath <user.email=vinnu4264@gmail.com>
2022-08-08 20:00:46 -0400

    Testing multithreading

--- application/app.py
+++ application/app.py
@@ -24,5 +24,5 @@ def sayHello(test):
     
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True, port=3000)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True, port=3000)
